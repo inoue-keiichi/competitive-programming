d712791c68be3115d2fbf2e96595df96cfe7ea00
inoue-keiichi <inouejai@gmail.com>
2023-06-19 01:12:28 +0900

    submit

--- src/hackerRank/index1.ts
+++ src/hackerRank/index1.ts
@@ -1,27 +1,30 @@
-"use strict";
+{
+  ("use strict");
 
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-let inputString: string = "";
-let inputLines: string[] = [];
-let currentLine: number = 0;
-process.stdin.on("data", function (inputStdin: string): void {
-  inputString += inputStdin;
-});
+  process.stdin.resume();
+  process.stdin.setEncoding("utf-8");
+  let inputString: string = "";
+  let inputLines: string[] = [];
+  let currentLine: number = 0;
+  process.stdin.on("data", function (inputStdin: string): void {
+    inputString += inputStdin;
+  });
 
-process.stdin.on("end", function (): void {
-  inputLines = inputString.split("\n");
-  inputString = "";
-  main();
-});
+  process.stdin.on("end", function (): void {
+    inputLines = inputString.split("\n");
+    inputString = "";
+    main();
+  });
 
-function readLine(): string {
-  return inputLines[currentLine++];
-}
+  function readLine(): string {
+    return inputLines[currentLine++];
+  }
 
-function main() {
-  // Enter your code here
-  const a = parseInt(readLine().replace(/\w+\s*=\s/, ""));
-  const b = parseInt(readLine().replace(/\w+\s*=\s/, ""));
-  return a + b;
+  function main() {
+    // Enter your code here
+    console.log("Hello");
+    const a = parseInt(readLine().replace(/\w+\s*=\s/, ""));
+    const b = parseInt(readLine().replace(/\w+\s*=\s/, ""));
+    return a + b;
+  }
 }
--- src/hackerRank/index4.ts
+++ src/hackerRank/index4.ts
@@ -1,63 +1,65 @@
-"use strict";
-
 import { WriteStream, createWriteStream } from "fs";
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
 
-let inputString: string = "";
-let inputLines: string[] = [];
-let currentLine: number = 0;
+{
+  ("use strict");
+  process.stdin.resume();
+  process.stdin.setEncoding("utf-8");
 
-process.stdin.on("data", function (inputStdin: string): void {
-  inputString += inputStdin;
-});
+  let inputString: string = "";
+  let inputLines: string[] = [];
+  let currentLine: number = 0;
 
-process.stdin.on("end", function (): void {
-  inputLines = inputString.split("\n");
-  inputString = "";
+  process.stdin.on("data", function (inputStdin: string): void {
+    inputString += inputStdin;
+  });
 
-  main();
-});
+  process.stdin.on("end", function (): void {
+    inputLines = inputString.split("\n");
+    inputString = "";
 
-function readLine(): string {
-  return inputLines[currentLine++];
-}
+    main();
+  });
 
-/*
- * Complete the 'diagonalDifference' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts 2D_INTEGER_ARRAY arr as parameter.
- */
-
-function diagonalDifference(arr: number[][]): number {
-  let sum1 = 0;
-  let sum2 = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    sum1 += arr[i][i];
-    sum2 += arr[i][arr.length - 1 - i];
-  }
-  return Math.abs(sum1 - sum2);
-}
-
-function main() {
-  const ws: WriteStream = createWriteStream("/dev/stdin");
-
-  const n: number = parseInt(readLine().trim(), 10);
-
-  let arr: number[][] = Array(n);
-
-  for (let i: number = 0; i < n; i++) {
-    arr[i] = readLine()
-      .replace(/\s+$/g, "")
-      .split(" ")
-      .map((arrTemp) => parseInt(arrTemp, 10));
+  function readLine(): string {
+    return inputLines[currentLine++];
   }
 
-  const result: number = diagonalDifference(arr);
+  /*
+   * Complete the 'diagonalDifference' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+   */
 
-  ws.write(result + "\n");
+  function diagonalDifference(arr: number[][]): number {
+    let sum1 = 0;
+    let sum2 = 0;
 
-  ws.end();
+    for (let i = 0; i < arr.length; i++) {
+      sum1 += arr[i][i];
+      sum2 += arr[i][arr.length - 1 - i];
+    }
+    return Math.abs(sum1 - sum2);
+  }
+
+  function main() {
+    const ws: WriteStream = createWriteStream("/dev/stdin");
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    let arr: number[][] = Array(n);
+
+    for (let i: number = 0; i < n; i++) {
+      arr[i] = readLine()
+        .replace(/\s+$/g, "")
+        .split(" ")
+        .map((arrTemp) => parseInt(arrTemp, 10));
+    }
+
+    const result: number = diagonalDifference(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+  }
 }
